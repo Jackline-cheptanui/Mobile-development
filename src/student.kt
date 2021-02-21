1c040bb875d26bcb5e381ae672c802d10c359c81
jackline <cheptanuiJackline@gmail.comgit config --global user.email cheptanuiJackline@gmail.com>
2021-02-21 10:34:32 +0300

    assignment_1

---
+++ src/student.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var fullname="daisycaroline"
+    println(fullname)
+    var age=20
+    println(age)
+    var phonenumber="0757702813"
+    println(phonenumber)
+    var weightinkg=50
+    println(weightinkg)
+    var citizen:Boolean=true
+    print(citizen)
+
+}
