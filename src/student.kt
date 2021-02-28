34f3bfc4e29c1adae151c1df5c2c62b4ac981663
jackline <cheptanuiJackline@gmail.comgit config --global user.email cheptanuiJackline@gmail.com>
2021-02-28 12:35:57 +0300

    assignment

--- src/student.kt
+++ src/student.kt
@@ -1,13 +1,79 @@
 fun main() {
-    var fullname="daisycaroline"
-    println(fullname)
-    var age=20
-    println(age)
-    var phonenumber="0757702813"
-    println(phonenumber)
-    var weightinkg=50
-    println(weightinkg)
-    var citizen:Boolean=true
-    print(citizen)
+    var name = "hello Jackline"
+    println(name)
+    var modulus = modulus(43, 20)
+    println(modulus)
+    var sum =addition(20,30,40,10)
+    println(sum)
+    interestingFact()
+}
+
+
+
+
+
+
+
+fun modulus(num1:Int,num2:Int):Int{
+    var modulus=num1%num2
+    return(modulus)
+
 
 }
+
+fun addition(a:Int,b:Int,c:Int,d:Int):Int {
+    var sum=a+b+c+d
+    return(sum)
+
+
+}
+
+fun interestingFact() {
+    val interestingFact=("curiosity")
+    println(interestingFact)
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
